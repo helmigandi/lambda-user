5fa9c97ef2868e75fe0adcdf1029b9303cffb5d2
Helmi Sugandi <sugandihelmi@gmail.com>
2021-04-03 10:38:58 +0700

    create download json function - check only support GET for httpMethod - read file first and after that set headers with attachment

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -24,7 +24,30 @@ class UserController {
   }
 
   static getUserJson(httpMethod) {
-    return "getUserJson";
+    if (httpMethod != "GET") {
+      return {
+        statusCode: 500,
+        message: "HTTP Method is Not Supported",
+      };
+    }
+    try {
+      let data = fs.readFileSync("/var/task/data/users.json");
+      data = JSON.parse(data);
+      return {
+        statusCode: 200,
+        headers: {
+          'Content-type' : 'application/json',
+          'Content-Disposition': 'attachment; filename=user-data.json'
+        },
+        values: data,
+      };
+    } catch (err) {
+      console.log("CATCH", err);
+      return {
+        statusCode: 500,
+        message: err.message,
+      };
+    }
   }
 }
 
